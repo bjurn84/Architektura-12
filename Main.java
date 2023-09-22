601167a26dbb6ddd6e95e31feaaf316ca58cb59d
bjurn84 <115032837+bjurn84@users.noreply.github.com>
2023-09-22 08:42:48 +0400

    HW

--- Main.java
+++ Main.java
@@ -1,108 +1,109 @@
-package UnitTest;
-
 public class Main {
+
     public static void main(String[] args) {
-        AccessRepoTest test = new AccessRepoTest();
-        test.getUser();
-        test.setUser();
-        test.getGroup();
-        test.setGroup();
-    }
-}
-
-class AccessRepo {
-    User user;
-    Group group;
-
-    public AccessRepo(User user, Group group) {
-        this.user = user;
-        this.group = group;
+        AccessRepoTest.runAllTests();
     }
 
-    public User getUser() {
-        return user;
-    }
+    public static class User {
+        private String name;
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+        public User(String name) {
+            this.name = name;
+        }
 
-    public Group getGroup() {
-        return group;
-    }
+        public String getName() {
+            return name;
+        }
 
-    public void setGroup(Group group) {
-        this.group = group;
-    }
-
-    public User findUser() {
-        return null;
-    }
-}
-
-class AccessRepoTest {
-
-    void getUser() {
-        User testUser = new User("NAME1");
-        AccessRepo accessRepoTest = new AccessRepo(testUser, new Group("Architektura"));
-        assertEquals(testUser, accessRepoTest.getUser());
-    }
-
-    void setUser() {
-        AccessRepo accessRepoTest = new AccessRepo(new User("NAME1"), new Group("Architektura"));
-        User testUser2 = new User("NAME2");
-        accessRepoTest.setUser(testUser2);
-        assertEquals(testUser2, accessRepoTest.getUser());
-    }
-
-    void getGroup() {
-        Group groupTest = new Group("Architektura");
-        AccessRepo accessRepoTest = new AccessRepo(new User("NAME1"), groupTest);
-        assertEquals(groupTest, accessRepoTest.getGroup());
-    }
-
-    void setGroup() {
-        AccessRepo accessRepoTest = new AccessRepo(new User("NAME1"), new Group("Architektura"));
-        Group testGroup = new Group("Архитерктура");
-        accessRepoTest.setGroup(testGroup);
-        assertEquals(testGroup, accessRepoTest.getGroup());
-    }
-
-    private void assertEquals(Object expected, Object actual) {
-        if (!expected.equals(actual)) {
-            throw new AssertionError("Expected: " + expected + ", but got: " + actual);
+        @Override
+        public boolean equals(Object obj) {
+            if(obj == this) {
+                return true;
+            }
+            if(!(obj instanceof User)) {
+                return false;
+            }
+            User other = (User) obj;
+            return (name != null && name.equals(other.name));
         }
     }
-}
 
-class Group {
-    String name;
+    public static class Group {
+        private String name;
 
-    public String getName() {
-        return name;
+        public Group(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj == this) {
+                return true;
+            }
+            if(!(obj instanceof Group)) {
+                return false;
+            }
+            Group other = (Group) obj;
+            return (name != null && name.equals(other.name));
+        }
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public static class AccessRepo {
+        private User user;
+        private Group group;
+
+        public AccessRepo(User user, Group group) {
+            this.user = user;
+            this.group = group;
+        }
+
+        public User findUser() {
+            return user;
+        }
+
+        public Group getGroup() {
+            return group;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+
+        public void setGroup(Group group) {
+            this.group = group;
+        }
     }
 
-    public Group(String name) {
-        this.name = name;
-    }
-}
+    public static class AccessRepoTest {
+        public static void runAllTests() {
+            testChangeUser();
+            testChangeGroup();
+        }
 
-class User {
-    String name;
+        public static void testChangeUser() {
+            AccessRepo accessRepo = new AccessRepo(new User("Name1"), new Group("Group1"));
+            User user2 = new User("Name2");
+            accessRepo.setUser(user2);
+            if(accessRepo.findUser().equals(user2)) {
+                System.out.println("Тест замены пользователя пройден!");
+            } else {
+                System.out.println("Тест замены пользователя провален!");
+            }
+        }
 
-    public User(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        public static void testChangeGroup() {
+            AccessRepo accessRepo = new AccessRepo(new User("Name1"), new Group("Архитектура ПО"));
+            Group newGroup = new Group("Software Arch");
+            accessRepo.setGroup(newGroup);
+            if(accessRepo.getGroup().equals(newGroup)) {
+                System.out.println("Тест замены группы пройден!");
+            } else {
+                System.out.println("Тест замены группы провален!");
+            }
+        }
     }
 }
